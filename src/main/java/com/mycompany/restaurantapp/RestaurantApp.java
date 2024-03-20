8b48fce3bff6a4ae347a4a9fe21791bf1f4eeb09
CamiiUribeee <acuribel@ufpso.edu.co>
2024-03-19 22:36:46 -0600

    Advances

--- src/main/java/com/mycompany/restaurantapp/RestaurantApp.java
+++ src/main/java/com/mycompany/restaurantapp/RestaurantApp.java
@@ -5,10 +5,19 @@ package com.mycompany.restaurantapp;
 import people.User; 
 import values.UserTypes; 
 
+
 public class RestaurantApp {
 
     public static void main(String[] args) {
-        User student = new User(UserTypes.STUDENT, "Fulanito", "2328");
-        student.sell();
+        User student = new User(UserTypes.STUDENT, "Camila", "192214");
+        student.buy(); 
+        student.sell();    // ahí le cambié el orden, primero buy y luego sell 
+        User administrative = new User (UserTypes.ADMINISTRATIVE, "María", "1928818281");
+        // administrative.sell(); ahí no podría poner ese .sell porque le diría a un administrativo que no está autorizado para vender 
+        administrative.SellingAdmin();
     }
+    
+  
+    
+    
 }
--- src/main/java/people/User.java
+++ src/main/java/people/User.java
@@ -1,6 +1,10 @@
 
 package people;
 import values.UserTypes;  // OJO CON IMPORTAR 
+import java.util.Scanner; 
+
+
+
 public class User {
     private UserTypes type; //importa para que no salga error, lo puse privado para que no haya problema (averiguar bien eso) 
     String name;
@@ -16,11 +20,32 @@ public class User {
     
     public void buy (){
         
+        System.out.println("Ingresa tu nombre de usuario");
+        Scanner read = new Scanner(System.in);
+        this.name = read.nextLine();
+        System.out.println("Ingresa tu código");
+        Scanner leer = new Scanner(System.in);    //ojo, ahí me dí cuenta que un scanner solo tiene un nombre, o sea, debes ir cambiando los nombres, tengo read y leer 
+        this.id= leer.nextLine(); 
     }
     public void sell (){
         if(this.type != UserTypes.ADMINISTRATIVE){   //si el tipo de usuario no es administrativo no puede vender 
-            System.out.println ("usted no està autorizado para vender");
+            System.out.println ("Usted no está autorizado para vender");
         }
-            
+        
+        // if(this.type = UserTypes.ADMINISTRATIVE){
+        //    System.out.println ("Persona Autorizada");
+        // }       //hacer eso sería redundante?        
     }
+    public void SellingAdmin (){
+        if(this.type == UserTypes.ADMINISTRATIVE){   //ahí como estás comparando un valor tienes que poner doble igual, porque sino el computador va a leer que estás asignando un valor y noooo 
+            System.out.println("Persona Autorizada"); 
+            
+        }
+    }
+    
+    
+    
+    
+    
+    
 }
